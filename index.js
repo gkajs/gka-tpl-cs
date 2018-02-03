144802ee775b42c1fbd13a5b40132ae441f636ed
joeyguo <lgsincy@gmail.com>
2018-02-03 15:33:49 +0800

    子目录中每一个文件夹都生成对应html、css

--- index.js
+++ index.js
@@ -6,8 +6,28 @@ module.exports = function (data, opts, tool) {
         frameduration = opts.frameduration;
 
     var names = tool.getNames();
-    
+
     tool.writeFile("gka.css", css(data, prefix, frameduration));
     tool.writeFile("gka.html", html(names, prefix));
+
+
+    // 子目录中每一个文件夹都生成对应html、css
+    var frames = data.frames,
+        keyMap = data.keyMap,
+        cssFileName = '';
+
+    if (!keyMap || Object.keys(keyMap).length === 1) return;
+
+    for (var key in keyMap) {
+        cssFileName = key + "-gka.css";
+    	tool.writeFile(cssFileName, css({
+    		frames: data.frames,
+    		keyMap: {
+    			[key] : keyMap[key]
+    		}
+    	}, prefix, frameduration));
+    	tool.writeFile(key + "-gka.html", html(names, prefix, cssFileName));
+    }
+
 };
 
